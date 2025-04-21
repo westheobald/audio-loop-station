fbc7accaf1210871b62155f51a584e18f53debe2
Wes Theobald <theobald.wes@gmail.com>
2025-04-21 12:39:59 -0700

    removed sample rate checking

--- src/audio/loop-info.ts
+++ src/audio/loop-info.ts
@@ -32,18 +32,9 @@ export default class LoopInfo {
       this.barLength *
       this.validateNumber(numberOfBars, 1, 32, 'Number of bars');
 
-    sampleRate = this.validateSampleRate(sampleRate);
     // remove any fractional amount of sample to get
     const samples = Math.floor(loopLength * sampleRate);
     this.loopLength = samples / sampleRate;
-  }
-  validateSampleRate(rate: number) {
-    const validSampleRates = [
-      8000, 11025, 16000, 22050, 44100, 48000, 88200, 96000, 176400, 192000,
-      352800, 384000,
-    ];
-    if (validSampleRates.includes(rate)) return rate;
-    throw Error(`Invalid sample rate (recieved: ${rate}).`);
   }
   validateNumber(num: number, min: number, max: number, description: string) {
     if (!Number.isInteger(num)) {
